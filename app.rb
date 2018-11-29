91a655251afd41dd4b186ed276ab3604fa905131
Christopher Cahill and Maly Phongsavanh <christopher@>
2018-11-29 09:47:52 -0800

    update rectangle checker

---
+++ app.rb
@@ -0,0 +1,22 @@
+require('sinatra')
+require('sinatra/reloader')
+require('pry')
+also_reload('lib/**/*.rb')
+require('./lib/rectangle')
+
+get('/') do
+  @description = "This application will determine whether a rectangle is a square or not."
+  erb(:input)
+end
+
+get('/output') do
+  @length = params.fetch("length")
+  @width = params.fetch("width")
+  rectangle = Rectangle.new(@length, @width)
+  if rectangle.square?
+    @string_to_display = "This is A square."
+  else
+    @string_to_display = "This is not a square."
+  end
+  erb(:output)
+end
